358c5f2c230dbf6e129ff2fdb82803a50ff86dea
hussinsuleiman <162766071+hussinsuleiman@users.noreply.github.com>
2026-08-06 11:13:46 -0400

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(list)
+
+        for f,t in tickets:
+            graph[f].append(t)
+
+        res = []
+
+        def visit(airport):
+            if not graph[airport]:
+                res.append(airport)
+                return
+            
+            while graph[airport]:
+                nxt = ''
+
+                for nei in graph[airport]:
+                    if not nxt or nei < nxt:
+                        nxt = nei
+                
+                graph[airport].remove(nxt)
+                visit(nxt)
+            
+            res.append(airport) 
+
+        visit('JFK')
+        return res[::-1]
