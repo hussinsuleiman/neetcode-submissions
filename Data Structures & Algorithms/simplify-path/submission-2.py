285f4614e1eb50c139ed63c336c6f4a8122530a6
hussinsuleiman <162766071+hussinsuleiman@users.noreply.github.com>
2026-08-14 15:15:25 -0400

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = ['']
+        files = path.split('/')
+        print(files)
+
+        for f in files:
+            if not f or f == '.':
+                continue
+            
+            if f == '..':
+                if len(stack) > 1:
+                    stack.pop()
+                continue
+            
+            stack.append(f)
+
+        return '/'.join(stack) if len(stack) > 1 else '/'
