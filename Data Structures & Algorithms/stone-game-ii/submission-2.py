177d52515db0690e3dde4292515de2675c29439e
hussinsuleiman <162766071+hussinsuleiman@users.noreply.github.com>
2026-08-18 17:18:42 -0400

    Add: stone-game-ii - submission-2

---
+++ Data Structures & Algorithms/stone-game-ii/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        suf = [0] * (n+1)
+
+        for i in range(n-1, -1, -1):
+            suf[i] = suf[i+1] + piles[i]
+
+        dp = [[0] * (n+1) for i in range(n+1)]
+
+        for i in range(n-1, -1, -1):
+            for j in range(1, n+1):
+                curSum = 0
+
+                for k in range(1, 2*j+1):
+                    if i+k-1 == n:
+                        break
+
+                    curSum += piles[i+k-1]
+                    dp[i][j] = max(dp[i][j], curSum + suf[i+k] - dp[i+k][max(k,j)])
+
+        return dp[0][1]
