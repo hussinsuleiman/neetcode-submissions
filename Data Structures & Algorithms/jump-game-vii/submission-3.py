696a539ff7bf866639bda5c6a3a42f2a79b45af5
hussinsuleiman <162766071+hussinsuleiman@users.noreply.github.com>
2026-08-19 10:21:07 -0400

    Add: jump-game-vii - submission-3

---
+++ Data Structures & Algorithms/jump-game-vii/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        queue = deque([0])
+        farthest = 0
+        seen = set([0])
+
+        while queue:
+            top = queue.popleft()
+
+            for i in range(max(farthest+1, top+minJump), min(top + maxJump + 1, n)):
+                if s[i] == '0':
+                    queue.append(i)
+                    seen.add(i)
+
+            farthest = top + maxJump
+        
+        return n-1 in seen
